e7d6875785b7430fd1e0d7344369dd8392502e36
DavidSoto5317 <urielsonic@gmail.com>
2020-08-05 21:35:51 -0600

    Se sube el algoritmo CRC-32 con su implementacion en emisor y receptor

--- emisor.py
+++ emisor.py
@@ -2,6 +2,7 @@
 import re
 import socket
 import pickle
+from bitarray import bitarray
 
 ### Funciones utilizadas para la implementacion de Hamming
 def VerRdn(m): 
@@ -41,9 +42,57 @@ def VerFnl(dat, nr):
         res = res + val*(10**i) 
     return int(str(res), 2)
 
+### Funciones utilizadas para la implementacion de CRC
+def sumarSegmentos(segmento1, segmento2):
+    carrier = '0'
+    suma = ''
 
-Mensaje=input("Ingrese el mensaje: ")
+    for item in range(7,-1,-1):
+        a = int(segmento1[item])
+        b = int(segmento2[item])
+        
+        resultadoAB = a + b + int(carrier)
+        if resultadoAB == 0:
+            suma = '0' + suma
+            carrier = '0'
+        elif resultadoAB == 1:
+            suma = '1' + suma
+            carrier = '0'
+        elif resultadoAB == 2:
+            suma = '0' + suma
+            carrier = '1'
+        elif resultadoAB == 3:
+            suma = '1' + suma
+            carrier = '1'
 
+    if carrier != '0':
+        suma = carrier + suma
+    return suma
+    
+def emisionCRC32(binario):
+    n = 8
+    arrayBinary = [binario[i:i+n] for i in range(0, len(binario), n)]
+    sumaSegmentos = '00000000'
+
+    for i in arrayBinary:
+        sumaSegmentos = sumarSegmentos(sumaSegmentos, i)
+
+        if len(sumaSegmentos) > 8:
+            sumaSegmentos = sumaSegmentos[1:]
+            sumaSegmentos = sumarSegmentos(sumaSegmentos, '00000001')
+
+    return xor(sumaSegmentos)
+    
+def xor(segmento):
+    resultado = ''
+    for i in segmento:
+        if i == '0':
+            resultado = resultado + '1'
+        else:
+            resultado = resultado + '0'
+
+    return resultado
+    
 ### Conexion por Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1',9000))
@@ -52,6 +101,7 @@ conexion, direccion = sock.accept()
 print("Conexion relaizada con exito de la direccion", str(direccion))
 
 ### Procesamiento de capas del mensaje a enviar
+Mensaje=input("Ingrese el mensaje: ")
 mensajeEnviar = ''
 
 ### Convertir mensaje de STR -> BIN
@@ -59,45 +109,81 @@ Binario=' '.join(map(bin,bytearray(Mensaje,'utf8')))
 Binario=Binario.replace("b","")
 Binario=Binario.replace(" ","")
 print("Mensaje en binario:", Binario)
-m = len(Binario) 
-r = VerRdn(m) 
-dat = PosRdn(Binario, r) 
-dat = VerPrd(dat, r) 
-print(dat)
-print("Error Data is " + dat)
-correction = VerFnl(dat, r) 
-print("El error se ecuentra en " + str(correction))
-print(dat)
+binarioDeteccion = Binario
 
-### Se ingresa el ruido al mensaje
-ruido=int(input("Desea agregar ruido al mensaje, 1.Si 2.No (Seleccion un numero): "))
-if ruido==1:
-    Lista=list(dat)
-    lista2=[]
-    print(Lista)
-    intervalo=int(input("Ingrese el intervalo del ruido: "))
-    intervalo2=0
-    for i in range(len(dat)):
-        intervalo2+=1
-        if intervalo2==intervalo:
-            lista2.append("1")
-            intervalo2=0
-        else:
-            lista2.append(Lista[i])
-            # Lista[i]=Mensaje.replace(Mensaje[i],"q")
-            # print(Lista)
-        #print(lista2)
-    objeto=""
-    objeto=objeto.join(lista2)
-    print("Ruido")
-    print(objeto)
-    print("Ruido")
-    correction = VerFnl(objeto, r) 
-    print("El error se ecuentra en  " + str(correction))
-    dat = objeto
+opcionita=int(input("Cual algoritmo desea probar, 1.Hamming 2.CRC-32 (Seleccion un numero): "))
+if opcionita == 1:
+    m = len(Binario) 
+    r = VerRdn(m) 
+    dat = PosRdn(Binario, r) 
+    dat = VerPrd(dat, r) 
+    print(dat)
+    print("Error Data is " + dat)
+    correction = VerFnl(dat, r) 
+    print("El error se ecuentra en " + str(correction))
+    print(dat)
 
-### Utilizamos pickle para hacer provecho del uso del paquete bitarray    
-mensajeEnviar = pickle.dumps(dat)
+    ### Se ingresa el ruido al mensaje
+    ruido=int(input("Desea agregar ruido al mensaje, 1.Si 2.No (Seleccion un numero): "))
+    if ruido==1:
+        Lista=list(dat)
+        lista2=[]
+        print(Lista)
+        intervalo=int(input("Ingrese el intervalo del ruido: "))
+        intervalo2=0
+        for i in range(len(dat)):
+            intervalo2+=1
+            if intervalo2==intervalo:
+                lista2.append("1")
+                intervalo2=0
+            else:
+                lista2.append(Lista[i])
+                # Lista[i]=Mensaje.replace(Mensaje[i],"q")
+                # print(Lista)
+            #print(lista2)
+        objeto=""
+        objeto=objeto.join(lista2)
+        print("Ruido")
+        print(objeto)
+        print("Ruido")
+        correction = VerFnl(objeto, r) 
+        print("El error se ecuentra en  " + str(correction))
+        dat = objeto
+
+    ### Utilizamos pickle para hacer provecho del uso del paquete bitarray    
+    dat = '0' + dat
+    mensajeEnviar = pickle.dumps(dat)
+
+else:
+    dat = Binario
+    ### Se ingresa el ruido al mensaje
+    ruido=int(input("Desea agregar ruido al mensaje, 1.Si 2.No (Seleccion un numero): "))
+    if ruido==1:
+        Lista=list(Binario)
+        lista2=[]
+        print(Lista)
+        intervalo=int(input("Ingrese el intervalo del ruido: "))
+        intervalo2=0
+        for i in range(len(Binario)):
+            intervalo2+=1
+            if intervalo2==intervalo:
+                lista2.append("1")
+                intervalo2=0
+            else:
+                lista2.append(Lista[i])
+        objeto=""
+        objeto=objeto.join(lista2)
+        print("Ruido")
+        print(objeto)
+        print("Ruido")
+        dat = objeto
+
+
+    complemento = emisionCRC32(dat)
+    mensaje = '1' + Binario + complemento
+
+    ### Utilizamos pickle para hacer provecho del uso del paquete bitarray    
+    mensajeEnviar = pickle.dumps(mensaje)
 
 ### Se envia el objeto mensaje
 conexion.send(mensajeEnviar)
--- receptor.py
+++ receptor.py
@@ -2,6 +2,75 @@
 import socket
 import pickle
 
+### Funciones utilizadas para la implementacion de CRC
+def sumarSegmentos(segmento1, segmento2):
+    carrier = '0'
+    suma = ''
+
+    for item in range(7,-1,-1):
+        a = int(segmento1[item])
+        b = int(segmento2[item])
+        
+        resultadoAB = a + b + int(carrier)
+        if resultadoAB == 0:
+            suma = '0' + suma
+            carrier = '0'
+        elif resultadoAB == 1:
+            suma = '1' + suma
+            carrier = '0'
+        elif resultadoAB == 2:
+            suma = '0' + suma
+            carrier = '1'
+        elif resultadoAB == 3:
+            suma = '1' + suma
+            carrier = '1'
+
+    if carrier != '0':
+        suma = carrier + suma
+    return suma
+    
+def recepcionCRC32(binario):
+    n = 8
+    arrayBinary = [binario[i:i+n] for i in range(0, len(binario), n)]
+    arrayBinary.pop()
+    sumaSegmentos = '00000000'
+
+    for i in arrayBinary:
+        sumaSegmentos = sumarSegmentos(sumaSegmentos, i)
+
+        if len(sumaSegmentos) > 8:
+            sumaSegmentos = sumaSegmentos[1:]
+            sumaSegmentos = sumarSegmentos(sumaSegmentos, '00000001')
+
+    return sumaSegmentos
+
+def verificador(segmento1, segmento2):
+    carrier = '0'
+    suma = ''
+
+    for item in range(7,-1,-1):
+        a = int(segmento1[item])
+        b = int(segmento2[item])
+        
+        resultadoAB = a + b + int(carrier)
+        if resultadoAB == 0:
+            suma = '0' + suma
+            carrier = '0'
+            return False
+        elif resultadoAB == 1:
+            suma = '1' + suma
+            carrier = '0'
+        elif resultadoAB == 2:
+            suma = '0' + suma
+            carrier = '1'
+            return False
+        elif resultadoAB == 3:
+            suma = '1' + suma
+            carrier = '1'
+            return False
+
+    return True
+
 ### Conexion por Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1',9000))
@@ -10,7 +79,26 @@ sock.connect(('127.0.0.1',9000))
 mensajeRecibido = sock.recv(1024)
 while mensajeRecibido:
     mensajeDesempacado = pickle.loads(mensajeRecibido)
+
+    algoritmo = mensajeDesempacado[0]
+    mensaje = mensajeDesempacado[1:]
     print(mensajeDesempacado)
+    if algoritmo == '0':
+        print('Verificacion Hamming')
+        print(mensaje) ## Este es el binario con ruido que vos generaste xD
+
+    else:
+        print('Verificacion CRC-32')
+        print(mensaje)
+
+        verificar = recepcionCRC32(mensaje)
+        segmento = mensaje[-8:]
+        resultado = verificador(verificar,segmento)
+
+        if resultado:
+            print('True')
+        else:
+            print('False')
 
     mensajeRecibido = sock.recv(1024)
 
